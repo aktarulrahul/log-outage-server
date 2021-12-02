aae2f5181be3c89b07de567a60eea86701683c45
aktarulrahul <contact.aktarul@gmail.com>
2021-12-02 09:37:10 +0600

    server init

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//MiddleWare
+app.use(cors());
+app.use(express.json());
+
+// MongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.${process.env.DB_C}.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function mongodbCURD() {
+  try {
+    /* ------------------------------------- 
+     checking connection with DB
+    ------------------------------------- */
+    await client.connect();
+    console.log('db connected');
+    /* ------------------------------------- 
+    database name and collection init
+    ------------------------------------- */
+    const database = client.db('logOutage');
+    const logCollection = database.collection('logs');
+  } finally {
+    // await client.close();
+  }
+}
+
+mongodbCURD().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Running Server');
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
